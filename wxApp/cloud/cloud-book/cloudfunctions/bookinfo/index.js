32adca9aa20ce69d0e21fb99660b0c1fa8d280d8
even-lq <lqnwd999@163.com>
2020-11-08 19:41:28 +0800

    canvas

--- wxApp/cloud/cloud-book/cloudfunctions/bookinfo/index.js
+++ wxApp/cloud/cloud-book/cloudfunctions/bookinfo/index.js
@@ -1,16 +1,16 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
+const rp = require('request-promise')
+
 
 cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+  const res = rp('https://api.douban.com/v2/nook/isbn/' + event.isbn).then(html => {
+    return html
+  }).catch(err => {
+    console.log(err);
+  })
+  return res
 }
