b241c4d855734dafd807eb4f9e9c02d4e394fe9d
even-lq <lqnwd999@163.com>
2020-12-12 11:03:24 +0800

    path

---
+++ algorithm/栈/简化路径/index.js
@@ -0,0 +1,9 @@
+let str = "/a//b////c/d//././/.."
+let str1 = ['a', 'b', 'c']
+console.log(str.split('/'));
+let str2 = str1.join('/')
+console.log(str2);
+
+let a = 'b'
+let b = 'c'
+console.log(a < b);
