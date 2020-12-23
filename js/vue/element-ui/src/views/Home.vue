85d533ec9c57903aa754b41a19daecc32f0a422d
even-lq <lqnwd999@163.com>
2020-12-23 14:02:45 +0800

    vue-elementui

---
+++ js/vue/element-ui/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">home, {{$route.query}}</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- js/vue/element-ui/src/views/Login.vue
+++ js/vue/element-ui/src/views/Login.vue
@@ -3,14 +3,42 @@
     <div class="login-container">
       <h2>旅梦后台管理系统</h2>
       <div class="wrap">
-
+        <el-input
+          placeholder="请输入用户名"
+          prefix-icon="el-icon-user"
+          v-model="account"
+        >
+        </el-input>
+        <el-input
+          placeholder="请输入密码"
+          prefix-icon="el-icon-lock"
+          v-model="password"
+          show-password
+        ></el-input>
+        <el-button type="primary" @click="login">登录</el-button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      account: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      if (this.account == "admin" && this.password == "123") {
+        this.$router.push({path: '/home', query: { user : this.account}});
+      } else {
+        alert("账号或密码错误！");
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,6 +48,19 @@ export default {};
 }
 .login-container {
   text-align: center;
-  line-height: 100vh;
+  width: 300px;
+  height: 260px;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  margin: auto;
+  box-shadow: 0px 0px 5px #999;
+  border-radius: 5px;
+}
+.wrap {
+  padding: 10px 50px;
+}
+input {
+  margin: 10px 0;
 }
 </style>
