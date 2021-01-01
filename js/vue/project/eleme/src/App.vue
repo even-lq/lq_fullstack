b55e8d36fb5069e8ed6e85b17014e291d611c3a3
even-lq <lqnwd999@163.com>
2021-01-01 23:02:08 +0800

    eleme

---
+++ js/vue/project/eleme/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header/Header'
+// import header from '@/components/header/Header'
+export default {
+  components: {
+    Header,
+    // 'v-header': header 组件标签重命名
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
